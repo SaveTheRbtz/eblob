d9f748cd205d7d762b65899350c02b2b380032eb
Alexey Ivanov <SaveTheRbtz@GMail.com>
2012-11-27 22:23:03 +0800

    l2hash: added mumrmur2 hash implementation

--- library/hash.c
+++ library/hash.c
@@ -44,6 +44,61 @@
 #include "hash.h"
 #include "blob.h"
 
+/*
+ * 64bit murmur implementation
+ * Known as MurmurHash64A
+ * TODO: Can be replaced with 32-bit version
+ */
+static uint64_t eblob_l2hash(const void *key, int len, uint64_t seed)
+{
+	const uint64_t m = 0xc6a4a7935bd1e995LLU;
+	const int r = 47;
+
+	uint64_t h = seed ^ (len * m);
+
+	const uint64_t *data = (const uint64_t *)key;
+	const uint64_t *end = data + (len/8);
+
+	while(data != end)
+	{
+		uint64_t k = *data++;
+
+		k *= m;
+		k ^= k >> r;
+		k *= m;
+
+		h ^= k;
+		h *= m;
+	}
+
+	const unsigned char *data2 = (const unsigned char*)data;
+
+	switch(len & 7) {
+	case 7: h ^= (uint64_t)data2[6] << 48;
+	case 6: h ^= (uint64_t)data2[5] << 40;
+	case 5: h ^= (uint64_t)data2[4] << 32;
+	case 4: h ^= (uint64_t)data2[3] << 24;
+	case 3: h ^= (uint64_t)data2[2] << 16;
+	case 2: h ^= (uint64_t)data2[1] << 8;
+	case 1: h ^= (uint64_t)data2[0];
+		h *= m;
+	};
+
+	h ^= h >> r;
+	h *= m;
+	h ^= h >> r;
+
+	return h;
+}
+
+/*
+ * Second level hash for eblob key
+ */
+static inline uint64_t eblob_l2hash_key(struct eblob_key *key)
+{
+	return eblob_l2hash(key, EBLOB_ID_SIZE, 0);
+}
+
 static void eblob_hash_entry_free(struct eblob_hash *h __unused, struct eblob_hash_entry *e)
 {
 	free(e);
