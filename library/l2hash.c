fed6c30912854c9491f02817d11986955474105f
Alexey Ivanov <SaveTheRbtz@GMail.com>
2012-11-29 05:39:51 +0800

    l2hash: added note about recursive eblob_l2hash_destroy()

--- library/l2hash.c
+++ library/l2hash.c
@@ -159,6 +159,8 @@ int eblob_l2hash_destroy(struct eblob_l2hash *l2h)
 	if (l2h == NULL)
 		return -EINVAL;
 
+	/* FIXME: Recursively destroy tree and lists */
+
 	err = pthread_mutex_destroy(&l2h->root_lock);
 	free(l2h);
 
